b9a2ee5ffbcca82c080da1cce31d5a9c5dffb6b1
Aomar Bariz <aomarb@yahoo.fr>
2024-07-22 11:40:11 +0200

    Update apply_semp_config.py

--- apply_semp_config.py
+++ apply_semp_config.py
@@ -13,7 +13,7 @@ semp_payload = {
         "egressEnabled": True,
         "ingressEnabled": True,
         "permission": "delete",
-        "queueName": "githubactions"
+        "queueName": "sanoif-new-queue"
 }
 
 def apply_semp_configuration():
